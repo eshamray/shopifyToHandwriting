f06f70ae7cf8a02179efbd2cec793b8ccf371327
e.shamray <okeanrst@gmail.com>
2019-08-20 18:17:39 +0300

    add auth form

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -7,19 +8,30 @@ const nonce = require('nonce')();
 const querystring = require('querystring');
 const request = require('request-promise');
 
-const apiKey = process.env.SHOPIFY_API_KEY;
-const apiSecret = process.env.SHOPIFY_API_SECRET;
-const scopes = 'read_products';
-//TODO
-const forwardingAddress = process.env.NGROK_FORWARDING_ADDRESS;
+const {
+  API_URL, TESTING = false, API_VERSION, SECRET_KEY,
+  SHOPIFY_API_KEY: apiKey, SHOPIFY_API_SECRET: apiSecret,
+  NGROK_FORWARDING_ADDRESS: forwardingAddress,
+} = process.env;
 
-app.get('/', (req, res) => {
-  console.log('res=Hello World!')
-  res.send('Hello World!');
+
+const scopes = 'read_products';
+
+
+app.use('/', express.static(path.resolve(__dirname, '../public')));
+
+// app.get('/', (req, res) => {
+//   console.log('res=Hello World!')
+//   res.send('Hello World!');
+// });
+
+//TODO will be done with google service   verifyPassword
+app.post('/auth', (req, res) => {
+  res.json({user: {api_key: 'api_key464546456'}})
 });
 
 app.get('/shopify', (req, res) => {
-  const shop = req.query.shop;debugger
+  const shop = req.query.shop;
   if (shop) {
     const state = nonce();
     const redirectUri = forwardingAddress + '/shopify/callback';
@@ -29,10 +41,78 @@ app.get('/shopify', (req, res) => {
       '&state=' + state +
       '&redirect_uri=' + redirectUri;
 
+    return res.redirect('/authForm.html');
+
     res.cookie('state', state);
     res.redirect(installUrl);
   } else {
     return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+});
+
+app.get('/shopify/callback', (req, res) => {
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    const map = Object.assign({}, req.query);
+    delete map['signature'];
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const providedHmac = Buffer.from(hmac, 'utf-8');
+    const generatedHash = Buffer.from(
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(message)
+        .digest('hex'),
+      'utf-8'
+    );
+    let hashEquals = false;
+
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    } catch (e) {
+      hashEquals = false;
+    };
+
+    if (!hashEquals) {
+      return res.status(400).send('HMAC validation failed');
+    }
+
+    //res.status(200).send('HMAC validated');
+    //TODO probably I need to exchange this on render template
+    res.redirect('/');
+    return;
+
+    //I don't if I need to use access_token to make direct requests from firebase to shopify service
+    //probably I need to return html page with input fields (campaign id)
+
+    //Exchange temporary code for a permanent access token
+    /*const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+    const accessTokenPayload = {
+      client_id: apiKey,
+      client_secret: apiSecret,
+      code,
+    };
+
+    request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+      .then((accessTokenResponse) => {
+        const accessToken = accessTokenResponse.access_token;
+
+        res.status(200).send("Got an access token, let's do something with it");
+        // TODO
+        // Use access token to make API call to 'shop' endpoint
+      })
+      .catch((error) => {
+        res.status(error.statusCode).send(error.error.error_description);
+      });*/
+
+  } else {
+    res.status(400).send('Required parameters missing');
   }
 });
 
