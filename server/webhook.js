d57621da499e3a6b049f89c267ebf9191606197a
e.shamray <okeanrst@gmail.com>
2019-08-27 14:30:51 +0300

    fix webhooks response

--- server/webhook.js
+++ server/webhook.js
@@ -11,29 +11,29 @@ router.post('/app/uninstalled', verifyWebhook, async (req, res) => {
     await Shop.remove({shop: domain, _id: innerShopId});
   } catch (error) {
     const webhook = 'on delete app';
-    console.error(`Webhook "${webhook}": Removing Shopify record error: ${error}`);
-    return res.sendStatus(500);
+    console.error(`Webhook "${webhook}": Removing Shopify record (shop: "${domain}", innerShopId: "${innerShopId}") error: ${error}`);
+    return res.sendStatus(500).json({success: false, error: 'Account was connected earlier' });
   }
 
-  return res.sendStatus(200);
+  return res.sendStatus(200).json({success: true});
 });
 
-router.post('/customers/create', verifyWebhook, async (req, res) => {debugger
-  const shop = '';
-
+router.post('/customers/create', verifyWebhook, async (req, res) => {
+  const { ['x-shopify-shop-domain']: shop } = req.headers;
+  const { id } = req.body;
   try {
     const shopRec = await Shop.findOne({shop});
     if (shopRec && shopRec.customerCreatedCampaignId && shopRec.userId) {
       //send handwriting post card
-      console.log(`Webhook: customers/create`);
+      console.log(`Webhook: customers/create id: ${id}`);
     }
   } catch (error) {
     //TODO figure out if on delete shop or on delete app
     const webhook = 'customers/create';
     console.error(`Webhook "${webhook}": Sending handwriting post card error: ${error}`);
-    return res.sendStatus(500);
+    return res.sendStatus(500).json({success: false});
   }
-  return res.sendStatus(200);
+  return res.sendStatus(200).json({success: true});
 });
 
 module.exports = router;
